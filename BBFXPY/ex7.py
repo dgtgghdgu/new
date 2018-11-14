b4a4b39110e244106ea6821ade11c0ad11b18c5f
ZhengPing Shi <dgtgghdgu@gmail.com>
2018-11-14 18:33:35 +0800

    exam7-9 for study

---
+++ BBFXPY/ex7.py
@@ -0,0 +1,21 @@
+print("Mary had a little lamb")
+print("It's fleece was white as %s." % 'snow' )
+print("And evevywhere that Mary went.")
+print("." * 10 ) #这是做什么，不就是打印10个......吗
+
+end1 ="C"
+end2 ="h"
+end3 ="e"
+end4 ="e"
+end5 ="s"
+end6 ="e"
+end7 ="B"
+end8 ="u"
+end9 ="r"
+end10 ="g"
+end11 ="e"
+end12 ="r"
+
+#看他们组合在一起是什么样子，如果打乱顺序又会发生什么
+print(end1 + end3 + end5 +end7 +end9 + end11 )
+print(end2 + end4 + end6 +end8 +end10 + end12)
