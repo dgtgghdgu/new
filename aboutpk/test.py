c343b56f7b7088d02405442811365e5d61227e0b
ZhengPing Shi <dgtgghdgu@gmail.com>
2018-11-21 16:32:37 +0800

    trying a easy game. so hard

---
+++ aboutpk/test.py
@@ -0,0 +1,5 @@
+import whoisbigger
+
+winner_count_list = []
+
+for i in range(10):
--- aboutpk/whoisbigger.py
+++ aboutpk/whoisbigger.py
@@ -1,9 +1,13 @@
 # -*- conding:Utf-8 -*-
 import random
+import pandas as pd
+import numpy as np
 
 class Player(object):
     def __init__(self):
         pass
+
+    card = None
 
     def get_card(self,count):
         #随机获得一张点数
@@ -17,20 +21,29 @@ def whoisbig(p1,p2):
     pass
 
 
-def deal(serial,player):
+def deal(serial, player) :
     serial_main =serial
-    rnd_int = random.randint(1,len(serial_main))
-    player_card = serial_main[rnd_int]
+    rnd_int = random.randint(0,(len(serial_main)-1))
+    player.card = serial_main[rnd_int]
     del(serial_main[rnd_int])
-    return serial_main,player_card
+    return serial_main,player.card
 
-def showdown(player1,player2):
+def showdown(p_card1,p_card2):
+    if int(p_card1[1:]) > int(p_card2[1:]):
+        win = p_card1
+        winner = "player 1"
+    elif int(p_card1[1:]) == int(p_card2[1:]):
+        win = "Tie"
+        winner = "Tie"
+    else:
+        win = p_card2
+        winner = "Player 2"
+
+    return win,winner
     
 
 
 main_poker_serial = []
-
-
 for i in range(13):
     for j in ["C","D","H","S"]:
         main_poker_serial.append(j+str(i+1))
@@ -38,23 +51,24 @@ for i in range(13):
 print(main_poker_serial)
 print(len(main_poker_serial))
 print("*" *30)
-
-#main_poker_serial_ready = random.shuffle(main_poker_serial)
-
-#print(main_poker_serial_ready)
-
-player_1 = Player()
-main_poker_serial,player_1_card = deal(main_poker_serial,player_1)
-print(len(main_poker_serial),main_poker_serial)
-print("*" *30)
-print(player_1_card)
+print(main_poker_serial[0:52])
 
 
 
-player_2 = Player()
-main_poker_serial,player_2_card = deal(main_poker_serial,player_2)
-print("*" *30)
-print(player_2_card)
+#print("%s 获胜。\n赢家是 %s。" % (showdown(player_1.card,player_2.card)))
 
+winner_count_list = []
 
+for i in range(100):
+    player_1 = Player()
+    main_poker_serial,player_1.card = deal(main_poker_serial, player_1)
 
+    player_2 = Player()
+    main_poker_serial,player_2.card = deal(main_poker_serial,player_2)
+
+    winner_count_list.append(showdown(player_1.card,player_2.card))
+
+print(winner_count_list)
+
+#winner_df = pd.DataFrame(winner_count_list)
+#print(winner_df)
